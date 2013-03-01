9bedd3bd7a702a37c46596334bab3ebb94349716
unknown <Administrator@hwchu-PC.(none)>
2013-03-01 16:25:41 +0800

    project create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helloworld::Application.config.secret_token = 'd4d9976ce7413e079b6801ed0c6ddbead7d14fa3e065d792932a3a07db90c9b5c582cd1c0b33d58c072b20fe55b1f9e7b4f4302552d2ef77c51bf4ab34ad49a2'
